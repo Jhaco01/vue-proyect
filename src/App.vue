4de5a0e2721773fafc7afc8fef26d65bd3b1ce5b
Jhacoby Delgado <jkeithd28@gmail.com>
2023-02-07 22:11:40 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,71 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <a @click="toggleCart" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"/>
+      <span>Cart {{ totalQuantity }}</span>
+    </a>
+  </header>
+  <router-view
+    :inventory="inventory"
+    :addToCart="addToCart"
+  />
+  <SidebarComponent
+    v-if="showCart"
+    :toggle="toggleCart"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
 </template>
+
+<script>
+import SidebarComponent from './components/SidebarComponent.vue'
+import food from '@/food.json'
+
+export default {
+  data () {
+    return {
+      showCart: false,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((sum, curr) => {
+        return sum + curr
+      }, 0)
+    }
+  },
+  methods: {
+    addToCart (name, i, quantity) {
+      if (!this.cart[name]) this.cart[name] = 0
+      this.cart[name] += quantity
+    },
+    toggleCart () {
+      this.showCart = !this.showCart
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  },
+  components: {
+    SidebarComponent
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/views/ProductView.vue
@@ -0,0 +1,24 @@
+<template>
+    <main class="wrapper">
+        <h1>Products</h1>
+        <ProductCard
+            v-for="(product, index) in inventory"
+            :key="product.id"
+            class="card"
+            :index="index"
+            :product="product"
+            :addToCart="addToCart"
+        />
+    </main>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
